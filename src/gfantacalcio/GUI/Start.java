59b037b501b22cc3d844f2e860b76aa9261685df
studente1 <studente1@PC-ma14>
2016-03-07 12:53:23 +0100

    ciao mesco

--- src/gfantacalcio/GUI/Start.java
+++ src/gfantacalcio/GUI/Start.java
@@ -9,18 +9,29 @@ public class Start extends JFrame{
 
 	private	JPanel topPanel;
 	private	JList listbox;
-        
+        private JButton crea;
+        private JButton crea1;
+        private JButton crea2;
+        private JButton crea3;
+        private JButton crea4;
 
 	public Start(){
 		
                 setTitle( "GFANTACALCIO" );
-		setSize( 1000, 500 );
-		topPanel = new JPanel();
-                topPanel.setLayout(new GridLayout(4, 2, 10, 10));
-		getContentPane().add( topPanel );
-
-		// Create some items to add to the list
-		String	listData[] =
+		setSize( 500, 300 );
+		
+                topPanel = new JPanel();
+                topPanel.setLayout(null);
+		
+                crea = new JButton();
+                crea.setBounds(300, 170, 150, 45);
+                
+		crea1 = new JButton();
+                crea2 = new JButton();
+                crea3 = new JButton();
+                crea4 = new JButton();
+		
+                String	listData[] =
 		{
 			"Item 1",
 			"Item 2",
@@ -29,8 +40,17 @@ public class Start extends JFrame{
 		};
 
 		listbox = new JList( listData );
+                listbox.setBounds(300, 50, 150, 100);
+                
+                topPanel.add(listbox);
+                topPanel.add(crea);
+                topPanel.add(crea1);
+                topPanel.add(crea2);
+                topPanel.add(crea3);
+                topPanel.add(crea4);
                 
                 setVisible(true);
-		topPanel.add( listbox, BorderLayout.CENTER );
-	}
+		
+                getContentPane().add( topPanel );
+        }
 }
